f1fccda6e7616541b15512c29bd1b35966bafd3a
IlkkoVili <vili.ilkko@gmail.com>
2023-05-01 15:28:36 +0300

    modified server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-
+var uri = process.env.DB_URI;
+const PORT = process.env.PORT || 5000;
 
 // Connect to database with mongoose
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
